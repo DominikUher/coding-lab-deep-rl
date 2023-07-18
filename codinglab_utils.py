36c1fcc389dd6c3b2011feeb3285495481c7f8a2
Dominik Uher <dominik.uher@me.com>
2023-07-18 15:08:50 +0200

    Added default PPO return for make_algorithm

--- codinglab_utils.py
+++ codinglab_utils.py
@@ -4,3 +4,5 @@ def make_algorithm(algorithm_name, hyperparameters, improvements):
     match algorithm_name:
         case 'PPO':
             return PPO.from_dict(hyperparameters)
+
+    return PPO.from_dict(hyperparameters)
