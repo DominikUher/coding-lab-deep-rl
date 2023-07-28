a8e18ebb962ebb101441152c8cc1831142656ee7
umitbaris <umit.gokkaya@boun.edu.tr>
2023-07-28 17:27:57 +0200

    visualization in single window

--- Visualization.py
+++ Visualization.py
@@ -5,29 +5,34 @@ import random
 # make sure to initialize the environment so that the (validation) episodes are the exact same as
 # the ones that achieved the high score
 from environment_CNN_moving import Environment
+
 env = Environment(variant=0, data_dir='./data')
+
 
 def visualize_episode(actions: list(), startAt):
     env.reset('validation', startAt)
     plt.ion()
+    fig, ax = plt.subplots()
+    total_reward = 0
     for step in range(env.episode_steps):
+        plt.cla()
+        ax.set_xticks(range(6), minor=False)
+        ax.set_yticks(range(6), minor=False)
+        ax.grid(which='both', color='black', linewidth=1)
+        ax.set_title('step: {}  total reward: {}'.format(step+1, total_reward))
         action = actions[step]
         agent_location = env.agent_loc
         target_location = env.target_loc
         item_locations = env.item_locs
         item_times = env.item_times
-        draw_grid(agent_location, target_location, item_locations, item_times)
-        plt.pause(1)
-        env.step(action)
+        draw_grid(agent_location, target_location, item_locations, item_times, fig, ax)
+        plt.pause(0.5)
+        reward, _, _ = env.step(action)
+
+        total_reward += reward
 
 
-def draw_grid(agent_location, target_location, item_locations, item_times):
-
-    fig, ax = plt.subplots()
-    ax.set_xticks(range(6), minor=False)
-    ax.set_yticks(range(6), minor=False)
-    ax.grid(which='both', color='black', linewidth=1)
-
+def draw_grid(agent_location, target_location, item_locations, item_times, fig, ax):
     agent_y, agent_x = agent_location
     target_y, target_x = target_location
     for i, tp in enumerate(item_locations):
@@ -38,13 +43,13 @@ def draw_grid(agent_location, target_location, item_locations, item_times):
         c2 = 248.0 / 255
         c3 = 255.0 / 255
         color = (c1, c2 * item_time, c3 * item_time)
-        rect = plt.Rectangle((item_x, item_y), 1, 1, facecolor=color)
+        rect = plt.Rectangle((item_x, item_y), 1, 1, facecolor=color, figure=fig)
         ax.add_patch(rect)
-    
-    target_rect = plt.Rectangle((target_x, target_y), 1, 1, facecolor=(0.2, 0.2, 0.2))
+
+    target_rect = plt.Rectangle((target_x, target_y), 1, 1, facecolor=(0.2, 0.2, 0.2), figure=fig)
     ax.add_patch(target_rect)
 
-    agent_rect = plt.Rectangle((agent_x+0.25, agent_y+0.25), 0.5, 0.5, facecolor="blue")
+    agent_rect = plt.Rectangle((agent_x + 0.25, agent_y + 0.25), 0.5, 0.5, facecolor="blue", figure=fig)
     ax.add_patch(agent_rect)
 
     plt.xlim(0, 5)
@@ -54,5 +59,10 @@ def draw_grid(agent_location, target_location, item_locations, item_times):
 
 if __name__ == '__main__':
     # example actions, replace with yours
-    actions = [3, 3, 4, 4, 4, 3, 1, 4, 0, 1, 3, 2, 1, 3, 2, 3, 4, 4, 3, 2, 4, 3, 0, 4, 4, 4, 4, 0, 4, 4, 0, 3, 3, 0, 2, 0, 0, 4, 4, 3, 4, 4, 0, 3, 1, 3, 3, 4, 4, 0, 1, 0, 3, 0, 1, 3, 3, 0, 4, 2, 4, 1, 4, 2, 4, 1, 4, 3, 2, 3, 1, 0, 4, 0, 4, 1, 1, 3, 1, 3, 3, 4, 1, 3, 3, 2, 3, 4, 1, 3, 4, 0, 3, 4, 4, 4, 3, 3, 3, 0, 3, 2, 3, 3, 1, 3, 4, 3, 3, 3, 3, 4, 3, 4, 4, 3, 2, 3, 2, 3, 1, 4, 3, 4, 1, 3, 3, 4, 0, 2, 1, 4, 0, 3, 1, 1, 2, 2, 4, 1, 3, 3, 4, 3, 4, 4, 0, 0, 1, 1, 4, 4, 3, 4, 3, 3, 4, 1, 0, 3, 4, 1, 3, 3, 2, 2, 3, 3, 2, 4, 2, 3, 0, 3, 4, 2, 1, 1, 2, 2, 3, 2, 2, 0, 4, 1, 4, 1, 4, 2, 0, 0, 3, 2, 0, 2, 1, 4, 0, 0]
+    actions = [3, 3, 4, 4, 4, 3, 1, 4, 0, 1, 3, 2, 1, 3, 2, 3, 4, 4, 3, 2, 4, 3, 0, 4, 4, 4, 4, 0, 4, 4, 0, 3, 3, 0, 2,
+               0, 0, 4, 4, 3, 4, 4, 0, 3, 1, 3, 3, 4, 4, 0, 1, 0, 3, 0, 1, 3, 3, 0, 4, 2, 4, 1, 4, 2, 4, 1, 4, 3, 2, 3,
+               1, 0, 4, 0, 4, 1, 1, 3, 1, 3, 3, 4, 1, 3, 3, 2, 3, 4, 1, 3, 4, 0, 3, 4, 4, 4, 3, 3, 3, 0, 3, 2, 3, 3, 1,
+               3, 4, 3, 3, 3, 3, 4, 3, 4, 4, 3, 2, 3, 2, 3, 1, 4, 3, 4, 1, 3, 3, 4, 0, 2, 1, 4, 0, 3, 1, 1, 2, 2, 4, 1,
+               3, 3, 4, 3, 4, 4, 0, 0, 1, 1, 4, 4, 3, 4, 3, 3, 4, 1, 0, 3, 4, 1, 3, 3, 2, 2, 3, 3, 2, 4, 2, 3, 0, 3, 4,
+               2, 1, 1, 2, 2, 3, 2, 2, 0, 4, 1, 4, 1, 4, 2, 0, 0, 3, 2, 0, 2, 1, 4, 0, 0]
     visualize_episode(actions, 121)
