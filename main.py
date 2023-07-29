8d210dd0441d05b0885c38900dbe78fb9fb881c6
Dominik Uher <dominik.uher@me.com>
2023-07-29 14:54:37 +0200

    Formatting

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ tf.random.set_seed(seed)
 # Declaring algorithm and environment parameters
 data_dir = './data'         # Only change if data directory changed
 variant = 0                 # Possible values: 0, 1, 2
-observation = 'NGreedy5'      # Possible values: 'Greedy', 'Neutral', 'ImageLike', 'NGreedyX' (with X in [1-9])
+observation = 'NGreedy5'    # Possible values: 'Greedy', 'Neutral', 'ImageLike', 'NGreedyX' (with X in [1-9])
 algorithm_name = 'PPO'      # Possible values: 'PPO', 'CNN_PPO'
 algorithm_improvements = {  # Choose which improvements to use
     'clip_ratio_annealing': True,
